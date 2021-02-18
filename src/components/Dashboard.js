faa59f8a7f360c5fce43b46e2230d9cc764e8fc2
elinapiispanen <elinapiispanen1@hotmail.fi>
2021-02-18 23:02:06 +0200

    example mui components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Content from './Content'
+import DashboardDrawer from './DashboardDrawer'
+
+function Dashboard() {
+   return (
+      <div>
+         moi
+        <DashboardDrawer/>
+        <Content/>
+      </div>
+   )
+}
+
+export default Dashboard
